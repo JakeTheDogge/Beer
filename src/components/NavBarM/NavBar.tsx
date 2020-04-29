a0731d5b1ed6cc8a3f6bb4ff5184e3a7877ba9aa
fedrfedra <vetervell@gmail.com>
2020-04-29 20:43:57 +0400

    navbat and logos

---
+++ src/components/NavBarM/NavBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavBar.css'
+
+
+
+export default function NavBar() {
+
+    return (
+        <div className='NavBarM'>
+                <button className='toProfile'></button>
+                <button className='writeButton'></button>
+        </div>
+    )
+}
