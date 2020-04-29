6c129a77ab8d5da5ce91c9fde8bbe30589d63b9a
fedrfedra <vetervell@gmail.com>
2020-04-29 20:43:26 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBarM/NavBar';
+import Conversations from './components/Conversations/Conversations';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const convs = [
+    {
+      name: 'Antosha',
+      id: 1
+    },
+    {
+      name: 'Vasya',
+      id: 2
+    },
+    {
+      name: 'Mom',
+      id: 3
+    }
+  ]
+    return (
+    <>
+    <NavBar/>
+    <Conversations conversations={convs}/>
+    </>
   );
 }
 
